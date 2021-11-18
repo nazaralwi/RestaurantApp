62e729adf8725c98e9af13292d09befc3543ce72
Nazar Alwi <alwinazar75@gmail.com>
2021-11-18 17:34:48 +0700

    Add `RestaurantItem` model

---
+++ RestaurantEngine/RestaurantItem.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantItem.swift
+//  RestaurantEngine
+//
+//  Created by Nazar Alwi on 18/11/21.
+//
+
+import Foundation
+
+struct RestaurantItem {
+    let id: UUID
+    let name: String
+    let description: String
+    let pictureId: Int
+    let city: String
+    let rating: Double
+}
