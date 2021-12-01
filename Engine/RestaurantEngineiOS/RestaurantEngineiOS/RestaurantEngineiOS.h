3fd65f498745eae08a364ef15935d911e9fbeee8
Nazar Alwi <alwinazar75@gmail.com>
2021-12-01 17:40:39 +0700

    Add `RestaurantEngineiOS` framework (prod and test) targets for iOS platform-specific components

---
+++ Engine/RestaurantEngineiOS/RestaurantEngineiOS/RestaurantEngineiOS.h
@@ -0,0 +1,18 @@
+//
+//  RestaurantEngineiOS.h
+//  RestaurantEngineiOS
+//
+//  Created by Nazar Alwi on 01/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RestaurantEngineiOS.
+FOUNDATION_EXPORT double RestaurantEngineiOSVersionNumber;
+
+//! Project version string for RestaurantEngineiOS.
+FOUNDATION_EXPORT const unsigned char RestaurantEngineiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RestaurantEngineiOS/PublicHeader.h>
+
+
