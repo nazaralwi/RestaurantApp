e30f87bb2d0b242a71e870e53ae122ea8ffb1197
Nazar Alwi <alwinazar75@gmail.com>
2021-12-02 10:45:36 +0700

    Add zero floating point

--- Prototype/Prototype/RestaurantItemViewModel+Prototype.swift
+++ Prototype/Prototype/RestaurantItemViewModel+Prototype.swift
@@ -21,7 +21,7 @@ extension RestaurantItemViewModel {
                 name: "Kafe Kita",
                 description: "Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc.",
                 location: "Gorontalo",
-                rating: "4",
+                rating: "4.0",
                 imageName: "image-1"
             ),
             RestaurantItemViewModel(
@@ -70,7 +70,7 @@ extension RestaurantItemViewModel {
                 name: "Gigitan Cepat",
                 description: "Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc.",
                 location: "Bali",
-                rating: "4",
+                rating: "4.0",
                 imageName: "image-8"
             ),
             RestaurantItemViewModel(
