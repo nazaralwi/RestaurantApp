b6191b5c194cb5ee480b814a51f53b2c3f68f168
Nazar Alwi <alwinazar75@gmail.com>
2021-12-02 10:26:13 +0700

    Render prototype restaurant data on the table view

--- Prototype/Prototype/RestaurantViewController.swift
+++ Prototype/Prototype/RestaurantViewController.swift
@@ -8,9 +8,25 @@
 import UIKit
 
 class RestaurantViewController: UITableViewController {
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { 10 }
+    private let restaurant = RestaurantItemViewModel.prototypeRestaurant
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { restaurant.count }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        tableView.dequeueReusableCell(withIdentifier: "RestaurantItemCell")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantItemCell", for: indexPath) as! RestaurantItemViewCell
+        let model = restaurant[indexPath.row]
+        cell.configure(with: model)
+        
+        return cell
+    }
+}
+
+extension RestaurantItemViewCell {
+    func configure(with model: RestaurantItemViewModel){
+        nameLabel.text = model.name
+        descriptionLabel.text = model.description
+        locationLabel.text = model.location
+        ratingLabel.text = model.rating
+        restaurantImageView.image = UIImage(named: model.imageName)
     }
 }
