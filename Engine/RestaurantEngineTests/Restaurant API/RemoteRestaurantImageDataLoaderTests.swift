0a3c776b344500473e753b7542304d45ca9520c7
Nazar Alwi <alwinazar75@gmail.com>
2021-12-28 10:31:08 +0700

    `RemoteRestaurantImageDataLoader` requests data from url

--- Engine/RestaurantEngineTests/Restaurant API/RemoteRestaurantImageDataLoaderTests.swift
+++ Engine/RestaurantEngineTests/Restaurant API/RemoteRestaurantImageDataLoaderTests.swift
@@ -6,24 +6,45 @@
 //
 
 import XCTest
+import RestaurantEngine
 
 class RemoteRestaurantImageDataLoader {
-    init(client: Any) {
-        
+    private let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func loadImageData(from url: URL) {
+        client.get(from: url) { _ in }
     }
 }
 
 class RemoteRestaurantImageDataLoaderTests: XCTestCase {
     func test_init_doesNotPerformAnyURLRequest() {
-        let client = ClientSpy()
+        let client = HTTPClientSpy()
         _ = RemoteRestaurantImageDataLoader(client: client)
         
         XCTAssertEqual(client.requestedURLs, [])
     }
     
+    func test_loadImageData_requestsDataFromURL() {
+        let client = HTTPClientSpy()
+        let sut = RemoteRestaurantImageDataLoader(client: client)
+        let url = URL(string: "https://a-url.dev/images/medium/")!
+        
+        sut.loadImageData(from: url)
+        
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
     // - MARK: Helpers
     
-    private class ClientSpy {
+    private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
+        
+        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+            requestedURLs.append(url)
+        }
     }
 }
